023305ef0cd9cefa418fc97bb668bf4d865bfa40
marcotrunc <marcotrunc@gmail.com>
2022-01-11 17:42:00 +0100

    added validation in jsnacks3

--- js/jsnacks3.js
+++ js/jsnacks3.js
@@ -2,16 +2,27 @@ console.log('JS OK!');
 /*Snack 3
 Il software deve chiedere per 10 volte all’utente di inserire un numero. 
 Il programma stampa la somma di tutti i numeri inseriti.*/
-// * Dichiarazioni variabili e richiamo elemento in pagina
-const listElement = document.getElementById('display');
-const userNumbers = [''];
+// * Richiamo elementi in pagina
+const displayElement = document.getElementById('display');
+
 let userSum = 0;
-// * Chiedere per 10 volte 
-for(let i = 0; i < 10; i++ ){
+let message;
+let isValid = true;
+// * Chiedere per 10 volte un numero da inserire
+for(let i = 0; i < 10; i++){
     const userElement = parseInt(prompt('Inserisci numero','2'));
     console.log(userElement);
-    userNumbers.push(userElement);
-    userSum += userElement
+    if(isNaN(userElement)){
+        isValid = false;
+    }else{
+        userSum += userElement;
+    }
+    
 }
-console.log(userNumbers);
-console.log(userSum);
+
+// * Validazione
+   if(isValid){
+       displayElement.innerText = `La somma finale è ${userSum}`;
+   } else{
+       displayElement.innerText ="Inserisci solo numeri!";
+   }
